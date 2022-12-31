70f4ccb508fad883273c5b26eb396eacf6fe34f4
Jonathan Gat <jonathangat91@gmail.com>
2022-12-31 17:14:46 +0000

    update api version

--- js/fetch.js
+++ js/fetch.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
 
     // fetch data
-    let api_url = "https://script.googleusercontent.com/macros/echo?user_content_key=djvxQtI9mBHhMJ34eR3PupTB1EGoWE_hUP5j531kwv3jHh9VDHTtDGVO6OcFgnnN6-mEuecB6x2yiXWSI-8qVFvKCsy3L1tum5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnNgJe5Cej5QpXntKME1HzzPF18DwR1g1OE_EavyzaRdGx5kjyLPlWT9ddoCBsxisbJctikAUYCIq0IKwhi5RD54AUOt1uM2vmtz9Jw9Md8uu&lib=M1DiwXD5nvlkjLar3N7xjR_Iw12cnwnuP";
+    let api_url = "https://script.google.com/macros/s/AKfycbyu4pDfnc9Vn4sVe7k9D1i8cJOQFgtF5L7deE7LuqU7lDbzQvK3yGF-1FJ3p1JXkXhjpA/exec";
    $.getJSON(api_url, function(data) {
 
         // populate date header
